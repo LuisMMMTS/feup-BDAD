a83c48678903492b82aae9c064daa815550896d5
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2019-05-21 15:29:31 +0100

    QUERY 6 DONE

--- Entrega 3/in6.sql
+++ Entrega 3/in6.sql
@@ -4,7 +4,5 @@
 
 --encontrar as datas de promoção de um produto especifico e o valor da promoção (PID, P.NAME, DATA_ININCO, DATA_FIM)
 SELECT P.id, P.name, PD.startDate, PD.finishDate
-FROM Product P JOIN ProductDiscount PD WHERE P.discountId = PD.id
-WHERE P.id like '%10020%'
-
---diferenca entre like e = ?
+FROM Product P JOIN ProductDiscount PD 
+WHERE P.discountId = PD.id AND P.id = 10020 AND PD.discount <> 0;
