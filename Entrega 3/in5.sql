623601ca85c61e876a9fa6add18f9b0c6bedd2b4
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2019-05-21 16:04:10 +0100

    QUERY 5 DONE

--- Entrega 3/in5.sql
+++ Entrega 3/in5.sql
@@ -5,12 +5,11 @@
 --Tabela para obter produtos mais bem classificados e a sua quantidade vendida
 SELECT * FROM
  (
-    SELECT P.id, P.name, sum(QP.quantity) as totalQuantity, PR.rating
-    FROM Product P JOIN ProductReview PR JOIN QuantityofProduct QP ON PR.productId = P.id AND QP.productId = P.id
-    WHERE 
+    SELECT P.id as id, P.name, avg(PR.rating) as rating, count(PR.rating) as numReview
+    FROM Product P JOIN ProductReview PR JOIN ON PR.productId = P.id
     GROUP BY P.id
     ORDER BY PR.rating DESC
 ) ProductQuantityReview
 
-WHERE ProductQuantityReview.PR.rating > 3
-ORDER BY ProductInfo.id;
+WHERE ProductQuantityReview.rating > 3
+ORDER BY ProductQuantityReview.id;
