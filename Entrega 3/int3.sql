bde5021e3586256b9dbe2699d15b2f9a139a8105
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2019-05-21 15:18:08 +0100

    QUERY 1 AND 3 DONE

--- Entrega 3/int3.sql
+++ Entrega 3/int3.sql
@@ -5,6 +5,6 @@
 --Encontra os 10 produtos menos vendidos
 SELECT QP.productId, P.name, sum(QP.quantity) as quantity
 FROM QuantityofProduct QP JOIN Product P ON P.id = QP.productId
-GROUP BY QP.productId;
+GROUP BY QP.productId
 ORDER BY quantity ASC
 LIMIT 10;
