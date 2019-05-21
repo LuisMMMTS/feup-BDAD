405fd98dbbb7dd5245098f0202b0a7d1b3a1f7f8
CMGS5 <37114874+CMGS5@users.noreply.github.com>
2019-05-21 17:18:10 +0100

    QUERY 8 START

--- Entrega 3/int8.sql
+++ Entrega 3/int8.sql
@@ -4,3 +4,9 @@
 
 --Paises onde sao feitas mais compras, valor dessas compras, numero de clientes
 --Ex(Portugal, 50.000$ vendas, 250 clientes)
+CREATE VIEW IF NOT EXISTS[Country_NumCostumers]
+	SELECT L.country, count(C.id) as numCustomers
+	FROM Location L JOIN CustomerLocation CL ON L.id = CL.locationId JOIN Customer C ON CL.customerId = C.id
+	GROUP BY L.country;
+
+	
