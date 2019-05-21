4b86c4694476ab078f3585703d56ff0871db6011
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2019-05-21 15:57:46 +0100

    QUERY 3 DONE

---
+++ Entrega 3/int3.sql
@@ -0,0 +1,10 @@
+.mode       columns
+.headers    on
+.nullvalue  NULL
+
+--Encontra os 10 produtos menos vendidos
+SELECT QP.productId, P.name, sum(QP.quantity) as quantity
+FROM QuantityofProduct QP JOIN Product P ON P.id = QP.productId
+GROUP BY QP.productId
+ORDER BY quantity ASC
+LIMIT 10;
