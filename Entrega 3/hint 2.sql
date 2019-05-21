b0a1382bd0df3626c2bf372443c59cdf503b465f
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2019-05-21 14:43:29 +0100

    Delete hint 2.sql

--- Entrega 3/hint 2.sql
+++
@@ -1,7 +0,0 @@
-.mode columns
-.headers on
-.nullvalue nullvalue
-
-SELECT Orders.CustomerId, sum(Orders.TotalPrice) as TotalPrice
-FROM Customer JOIN Orders ON Customer.id = Orders.CustomerId
-GROUP BY Orders.CustomerId;
