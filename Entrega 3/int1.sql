a161374191e87f29fcdcb0bb3b425e569a47e139
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2019-05-21 16:06:43 +0100

    QUERY 1 DONE

--- Entrega 3/int1.sql
+++ Entrega 3/int1.sql
@@ -5,7 +5,7 @@
 --Diz quantas orders e o valor destas orders de cada clientes numa dada data
 SELECT * FROM
  (
-    SELECT C.id, CD.Name, COUNT(*) as nOrders, sum(O.totalPrice) as totalPrice
+    SELECT C.id, CD.Name, count(*) as nOrders, sum(O.totalPrice) as totalPrice
     FROM Customer C JOIN Orders O JOIN CustomerDetails CD ON C.id = O.customerId AND C.ssn = CD.ssn
     WHERE '2018-07-12'<= O.orderDate AND O.orderDate<='2019-01-01' -- data pode ser alterada
     GROUP BY C.id
