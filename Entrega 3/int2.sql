8317f2604876fb87adbf578e9140d7acb7d69274
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2019-05-21 15:53:34 +0100

    QUERY 2 DONE

--- Entrega 3/int2.sql
+++ Entrega 3/int2.sql
@@ -2,16 +2,10 @@
 .headers on
 .nullvalue nullvalue
 
---Quantas vez que um cliente especificado comprou um mesmo produto; atributos( product id, product name, quantitidade)
-
-SELECT QP.productId, P.name, sum(QP.quantity) as totalQuantity, C.id
+--Quantas vezes um cliente especificado comprou um mesmo produto; atributos( product id, product name, quantitidade)
+SELECT QP.productId, P.name, sum(QP.quantity) as totalQuantity
 FROM QuantityofProduct QP JOIN Product P JOIN Orders O JOIN Customer C
-    ON QP.productId = P.id AND  O.customerId = C.id
-WHERE C.id like '%123520%'
-GROUP BY C.id
-ORDER BY quantity DESC
-
---problema: orders feitas pelo mesmo cliente do mesmo produto... 
---- obj : somar essas quantidades 
-
-
+    ON QP.productId = P.id AND  O.Id = QP.orderId
+WHERE O.customerId = 123515 -- pode ser alterado
+GROUP BY P.id
+ORDER BY totalQuantity DESC;
