2fb065b32a364255cd6f3b8835b6572787051427
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2019-05-22 22:43:46 +0100

    QUERY 8 DONE

--- Entrega 3/int8.sql
+++ Entrega 3/int8.sql
@@ -5,7 +5,8 @@
 --Paises onde sao feitas mais compras, valor dessas compras, numero de clientes
 --Ex(Portugal, 50.000$ vendas, 250 clientes)
 SELECT L.country, count(O.id) as numOrders
-FROM Location L JOIN CustomerLocation CL ON L.id = CL.locationId JOIN Orders O ON CL.customerId = O.customerId
+FROM Location L JOIN CustomerLocation CL ON L.id = CL.locationId 
+		JOIN Orders O ON CL.customerId = O.customerId
 GROUP BY L.country
 ORDER BY numOrders DESC;
 
