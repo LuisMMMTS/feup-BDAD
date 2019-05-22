0a73e356f2726545296f5bde4d892d1511324bfa
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2019-05-22 20:07:25 +0100

    must test query 9

---
+++ Entrega 3/int9.sql
@@ -0,0 +1,20 @@
+.mode       columns
+.headers    on
+.nullvalue  NULL
+
+-- produto mais vendido por Pais (Nome, P.Id, quantidade)
+
+SELECT * FROM
+ (
+    SELECT L.id, L.country, P.id , P.name, sum(QP.quantity) as totalQuantity
+    FROM Location L JOIN CustomerLocation CL ON L.id = CL.locationId
+                    JOIN Customer C  ON C.id = CL.customerId 
+                    JOIN Order O  ON Order.customerId = C.id
+                    JOIN QuantityOfProduct QP ON QP.orderId = O.id
+                    JOIN Product P On P.id = QP.productId
+    WHERE L.country like 'P%' -- country starting with P
+    GROUP BY L.id, P.id -- group by country and then by product -> all var's in group by bmust be in select
+    ORDER BY country ASC
+) N
+WHERE N.totalQuantity > 0
+ORDER BY N.id;
