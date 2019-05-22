12078f8ef620c19d457eb86b8c5207f76a26ebc6
CMGS5 <37114874+CMGS5@users.noreply.github.com>
2019-05-22 22:33:14 +0100

    QUERY 8 DONE

--- Entrega 3/int9.sql
+++ Entrega 3/int9.sql
@@ -9,7 +9,7 @@ SELECT * FROM
     SELECT L.id, L.country, P.id , P.name, sum(QP.quantity) as totalQuantity
     FROM Location L JOIN CustomerLocation CL ON L.id = CL.locationId
                     JOIN Customer C  ON C.id = CL.customerId 
-                    JOIN Order O  ON Order.customerId = C.id
+                    JOIN Orders O  ON O.customerId = C.id
                     JOIN QuantityOfProduct QP ON QP.orderId = O.id
                     JOIN Product P On P.id = QP.productId
     WHERE L.country like 'P%' -- country starting with P
