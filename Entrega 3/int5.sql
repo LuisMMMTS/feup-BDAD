5728cca316c4dcd7bfb832751a80307304ef4a66
CMGS5 <37114874+CMGS5@users.noreply.github.com>
2019-05-23 13:57:14 +0100

    QUERY 5 DONE

--- Entrega 3/int5.sql
+++ Entrega 3/int5.sql
@@ -5,8 +5,8 @@
 --Tabela para obter produtos mais bem classificados e a sua quantidade vendida
 SELECT * FROM
  (
-    SELECT P.id as id, P.name, avg(PR.rating) as rating, count(PR.rating) as numReview
-    FROM Product P JOIN ProductReview PR JOIN ON PR.productId = P.id
+    SELECT P.id as id, P.name, avg(PR.rating) as rating, count(PR.rating) as numReviews
+    FROM Product P JOIN ProductReview PR ON P.id = PR.productId
     GROUP BY P.id
     ORDER BY PR.rating DESC
 ) ProductQuantityReview
