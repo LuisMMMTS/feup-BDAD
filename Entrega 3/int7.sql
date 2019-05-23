54ef8fcf86ce64665ed6dc4d122b276d5edada3c
CMGS5 <37114874+CMGS5@users.noreply.github.com>
2019-05-23 13:53:12 +0100

    QUERY 7 DONE

--- Entrega 3/int7.sql
+++ Entrega 3/int7.sql
@@ -3,30 +3,26 @@
 .nullvalue  NULL
 
 --ProductId ; ProductName ; Quantity
-CREATE VIEW [ProductSales_WITHOUT_discount] as
+CREATE VIEW IF NOT EXISTS [ProductSales_WITHOUT_discount] as
     SELECT P.id, P.name, sum(QP.quantity) as QuantityNoDiscount
-    FROM Product P JOIN ProductDiscount PD JOIN QuantityOfProduct QP
-    ON PD.id = P.discountId AND QP.productId = P.id
-    WHERE PD.discount like '%0%'
+    FROM Product P JOIN ProductDiscount PD JOIN QuantityOfProduct QP JOIN Orders OD
+    ON PD.id = P.discountId AND QP.productId = P.id AND QP.orderId = OD.id
+    WHERE PD.discount = 0 OR OD.orderDate < PD.startDate OR OD.orderDate > PD.finishDate
     GROUP BY P.id
-    Order By P.id ASC
+    Order By P.id ASC;
 
 
 --ProductId ; ProductName ; Quantity
-CREATE VIEW [ProductSales_WITH_discount] as
+CREATE VIEW IF NOT EXISTS [ProductSales_WITH_discount] as
     SELECT P.id, P.name, sum(QP.quantity) as QuantityDiscount
-    FROM Product P JOIN ProductDiscount PD JOIN QuantityOfProduct QP
-    ON PD.id = P.discountId AND QP.productId = P.id
-    WHERE PD.discount > 0
+    FROM Product P JOIN ProductDiscount PD JOIN QuantityOfProduct QP JOIN Orders OD
+    ON PD.id = P.discountId AND QP.productId = P.id AND QP.orderId = OD.id
+    WHERE PD.discount > 0 AND OD.orderDate > PD.startDate AND OD.orderDate < PD.finishDate
     GROUP BY P.id
-    Order By P.id ASC
+    Order By P.id ASC;
 
 --comparar o numero de compras para cada produto quando é aplicada uma promoção e quando não é
-SELECT ProductSales_WITH_discount.P.id, 
-    ProductSales_WITH_discount.P.name
-    ProductSales_WITH_discount.QuantityNoDiscount,
-    ProductSales_WITHOUT_discount.QuantityDiscount
-
+SELECT *
 FROM ProductSales_WITH_discount NATURAL JOIN ProductSales_WITHOUT_discount
-GROUP BY ProductSales_WITH_discount.P.id
-ORDER BY ProductSales_WITH_discount.P.id
+GROUP BY ProductSales_WITH_discount.id
+ORDER BY ProductSales_WITH_discount.id;
