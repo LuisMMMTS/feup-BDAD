2714a17ca0cc14cdce8da963b79e6dd79f625df6
CMGS5 <37114874+CMGS5@users.noreply.github.com>
2019-05-23 13:57:38 +0100

    QUERY 6 DONE

--- Entrega 3/int6.sql
+++ Entrega 3/int6.sql
@@ -3,6 +3,8 @@
 .nullvalue  NULL
 
 --encontrar as datas de promoção de um produto especifico e o valor da promoção (PID, P.NAME, DATA_ININCO, DATA_FIM)
-SELECT P.id, P.name, PD.startDate, PD.finishDate
+SELECT P.id, P.name, PD.startDate, PD.finishDate, PD.discount
 FROM Product P JOIN ProductDiscount PD 
-WHERE P.discountId = PD.id AND P.id = 10020 AND PD.discount <> 0;
+WHERE P.discountId = PD.id AND P.id = 10100 AND PD.discount <> 0;
+
+--diferenca entre like e = ?
