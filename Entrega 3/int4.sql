3a9a52040bf52f5f30f0c618ad2bf1ec8b8b08c9
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2019-05-23 22:43:31 +0100

    QUERY 4 DONE

--- Entrega 3/int4.sql
+++ Entrega 3/int4.sql
@@ -5,21 +5,24 @@
 --Clientes que mais pouparam com descontos (CustomerId, Valor Total compras sem desconto, Valor poupado)
 
 
---Tabela com productId, preço, valor do desconto
+--Tabela com productId, preço, OrderDate, quantidade
 CREATE VIEW IF NOT EXISTS [Customer_Product] as
-SELECT C.id as clientId, QP.productId, P.price, O.orderDate, sum(QP.quantity) as totalQuantity
+SELECT C.id as clientId, QP.productId, P.price as price, O.orderDate, sum(QP.quantity) as totalQuantity
 FROM QuantityofProduct QP JOIN Product P ON QP.productId = P.id
 						  JOIN Orders O  ON O.Id = QP.orderId
 						  JOIN Customer C ON O.customerId = C.id
 GROUP BY O.id;
 
+--Tabela com productId, preço, valor do desconto
 CREATE VIEW IF NOT EXISTS [Product_and_Discount] as
-   SELECT P.id as productId, PD.id as discountId, PD.startDate, PD.finishDate, PD.discount
+   SELECT P.id as productId, PD.id as discountId, PD.discount as discount, PD.startDate, PD.finishDate
    FROM Product P JOIN ProductDiscount PD ON PD.id = P.discountId;
 
-SELECT *
-FROM Customer_Product NATURAL JOIN Product_and_Discount;
-
+SELECT Customer_Product.clientId, sum(Customer_Product.price*Customer_Product.discount) as discountedSum
+FROM Customer_Product NATURAL JOIN Product_and_Discount
+GROUP BY Customer_Product.clientId
+HAVING discountedSum > 0
+ORDER BY discountedSum DESC;
 
 
 
