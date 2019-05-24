edec241fd8335c5b1bf59a935fd65c8981fdaed8
Luís M T Silva <44646310+LuisMMMTS@users.noreply.github.com>
2019-05-25 00:34:19 +0100

    TRIGGER 1_DONE

--- Entrega 3/gatilho1_adiciona.sql
+++ Entrega 3/gatilho1_adiciona.sql
@@ -8,5 +8,12 @@ CREATE TRIGGER calculateOrderPrice
 AFTER INSERT ON QuantityOfProduct
 BEGIN
 UPDATE Orders
-SET totalPrice = totalPrice + New.quantity * (SELECT Product.price FROM Product WHERE Product.id = New.productId);
+SET totalPrice = totalPrice + round(New.quantity * (
+    SELECT Product.price
+        FROM Product 
+        WHERE Product.id = New.productId
+        )*(1-(
+        SELECT ProductDiscount.discount FROM ProductDiscount 
+        WHERE ProductDiscount.id=(SELECT Product.discountId FROM Product WHERE Product.id = New.productId))),2)
+WHERE Orders.Id=New.orderID;
 END;
---
+++ Entrega 3/gatilho1_verifica.sql
@@ -0,0 +1,9 @@
+.mode       columns
+.headers    on
+.nullvalue  NULL
+
+SELECT totalPrice FROM Orders WHERE Orders.id=20;
+
+INSERT INTO QuantityOfProduct VALUES (10025, 20, 1);
+
+SELECT totalPrice FROM Orders WHERE Orders.id=20;
