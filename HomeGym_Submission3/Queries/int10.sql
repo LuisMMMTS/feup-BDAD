b40f69023bc7ab7c3f41918bf69b3d366f0c0bd7
CMGS5 <37114874+CMGS5@users.noreply.github.com>
2019-05-26 15:13:59 +0100

    query 10 done

--- HomeGym_Submission3/Queries/int10.sql
+++ HomeGym_Submission3/Queries/int10.sql
@@ -2,13 +2,32 @@
 .headers    on
 .nullvalue  NULL
 
-SELECT exerciseID, Exercise.name, count(*)*numRepetitions*numSets as ExercisePopularity, nickname as Creator
-FROM Execution
-NATURAL JOIN Challenge
-NATURAL JOIN ExercisePlan
-NATURAL JOIN ExerciseParameters
-JOIN Exercise USING(ExerciseID)
-NATURAL JOIN CustomExercise
-NATURAL JOIN User
-GROUP BY exerciseID
-ORDER BY ExercisePopularity DESC;
+--top 10 costumers com maior taxa review/compra (C.id, C.name, nºcompras, nºreview, taxa)
+
+SELECT customerId, cName, nProducts, nReviews, (nProducts/nReviews) as productReviewRate FROM
+ (
+    SELECT C.id as customerId, CD.name as cName, count(QP.productId) as nProducts
+    FROM Customer C JOIN CustomerDetails CD ON C.ssn = CD.ssn
+                    JOIN Orders O ON C.id = O.customerId
+					JOIN QuantityofProduct QP ON O.id = QP.orderId
+    GROUP BY customerId
+    ORDER BY customerId ASC
+ ) N
+
+JOIN
+
+ (
+    SELECT C.id as id, CD.name as customerName, count(PR.customerId) as nReviews
+    FROM Customer C JOIN CustomerDetails CD ON C.ssn = CD.ssn
+                    JOIN ProductReview PR ON PR.customerId = id
+    
+    GROUP BY id
+    ORDER BY id ASC
+
+ ) M
+
+GROUP BY customerId
+HAVING N.nProducts > 0
+ORDER BY productReviewRate DESC
+LIMIT 10;
+
