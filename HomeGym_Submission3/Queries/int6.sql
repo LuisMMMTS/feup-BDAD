cc43d8aad4e8cb939a3966a376168403d03eb5b9
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2019-05-26 18:04:06 +0100

    Delete int6.sql

--- HomeGym_Submission3/Queries/int6.sql
+++
@@ -1,8 +0,0 @@
-.mode       columns
-.headers    on
-.nullvalue  NULL
-
-SELECT challengeID, avg(finalPlanRating) as Rating, count(*) as nVotes
-FROM Challenge NATURAL JOIN ParticipationDetails
-GROUP BY challengeID
-ORDER BY Rating DESC, nVotes DESC;
