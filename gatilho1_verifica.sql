80a97f5de5b7eb85d245dc1ced98dc9fd99ed792
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2019-05-26 17:19:45 +0100

    Delete gatilho1_verifica.sql

--- gatilho1_verifica.sql
+++
@@ -1,9 +0,0 @@
-.mode       columns
-.headers    on
-.nullvalue  NULL
-
-SELECT totalPrice FROM Orders WHERE Orders.id=20;
-
-INSERT INTO QuantityOfProduct VALUES (10025, 20, 1);
-
-SELECT totalPrice FROM Orders WHERE Orders.id=20;
