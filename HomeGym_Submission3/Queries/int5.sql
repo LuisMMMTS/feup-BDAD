448ef2d5356175133117c4f56a7b35e7e388b070
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2019-05-26 18:04:13 +0100

    Delete int5.sql

--- HomeGym_Submission3/Queries/int5.sql
+++
@@ -1,9 +0,0 @@
-.mode       columns
-.headers    on
-.nullvalue  NULL
-
-SELECT userID, nickname, userScore, COUNT(*) as nChallenges
-FROM User NATURAL JOIN ParticipationDetails
-GROUP BY userID
-ORDER BY userScore DESC, nChallenges DESC
-LIMIT 3;
