4d3dfeaff3d3346c51d3bad11df2741318dad12d
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2019-05-26 17:17:24 +0100

    GATILHOS

--- Entrega 3/gatilho4_adiciona.sql
+++ Entrega 3/gatilho4_adiciona.sql
@@ -7,16 +7,18 @@
 CREATE TRIGGER review_check
 AFTER INSERT ON ProductReview
 BEGIN
-SELECT Q.orderID
-    FROM QuantityOfProduct as Q
-    WHERE QuantityOfProduct.productID=new.productID;
-SELECT O.id 
-    FROM Orders as O
-    WHERE Orders.customerId=new.customerId;
+SELECT COUNT(*) AS contagem FROM (
+SELECT Q.orderId, O.id
+    FROM QuantityOfProduct Q JOIN Orders O ON O.id=Q.orderId
+    WHERE Q.productId=New.productId AND O.customerId=New.customerId) aaaa
 
-SELECT COUNT(*) FROM O.id NATURAL JOIN Q.orderID;
 
-contagem =  CASE WHEN contagem = 0 THEN ROLLBACK END;
+SELECT aaaa.contagem
+CASE
+    WHEN contagem =0  
+        THEN ROLLBACK 
+        END
+        FROM aaaa.contagem 
 
 
  end;
---
+++ Entrega 3/gatilho4_verifica.sql
@@ -0,0 +1,10 @@
+.mode columns
+.headers on
+.nullvalue nullvalue
+
+
+SELECT customerId from ProductReview;
+INSERT INTO ProductReview VALUES (123517,10014,5,'2017-5-16');
+SELECT customerId from ProductReview;
+INSERT INTO ProductReview VALUES (123517,10013,5,'2017-5-16');
+SELECT customerId from ProductReview;
